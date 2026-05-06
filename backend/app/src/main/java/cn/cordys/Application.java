2f36af1c6a57b9c558ab5174c867804605ae0ea2
hql <912003455@qq.com>
2026-05-06 17:49:05 +0800

    优化系统配置文件

--- backend/app/src/main/java/cn/cordys/Application.java
+++ backend/app/src/main/java/cn/cordys/Application.java
@@ -6,19 +6,11 @@ import org.springframework.boot.autoconfigure.ldap.LdapAutoConfiguration;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jAutoConfiguration;
 import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(exclude = {
         QuartzAutoConfiguration.class,
         LdapAutoConfiguration.class,
         Neo4jAutoConfiguration.class
-})
-@PropertySources({
-        @PropertySource(value = "classpath:commons.properties", encoding = "UTF-8", ignoreResourceNotFound = true),
-        @PropertySource(value = "classpath:commons.properties.${spring.profiles.active:dev}", encoding = "UTF-8", ignoreResourceNotFound = true),
-        @PropertySource(value = "classpath:cordys-crm.properties", encoding = "UTF-8", ignoreResourceNotFound = true),
-        @PropertySource(value = "classpath:cordys-crm.properties.${spring.profiles.active:dev}", encoding = "UTF-8", ignoreResourceNotFound = true),
 })
 @ServletComponentScan
 public class Application {
